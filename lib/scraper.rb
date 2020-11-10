18175fa1c8ead9468fe953d95e2e3e3505c5c204
Will Studabaker <will.studabaker@gmail.com>
2020-11-10 15:03:59 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -6,6 +6,7 @@ class Scraper
   def self.scrape_index_page(index_url)
     html = open(index_url)
     doc = Nokogiri::HTML(html)
+    students = doc.css("")
   end
 
   def self.scrape_profile_page(profile_url)
