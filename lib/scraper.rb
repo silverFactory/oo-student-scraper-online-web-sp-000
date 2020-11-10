ccbcc4e681d243b2d6ab8fb7da329fb43157241a
Will Studabaker <will.studabaker@gmail.com>
2020-11-10 16:59:08 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -26,4 +26,4 @@ class Scraper
   end
 
 end
-Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+puts Scraper.scrape_index_page("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
