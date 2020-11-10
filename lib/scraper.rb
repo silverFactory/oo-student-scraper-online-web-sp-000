85227e484b735d7b6f41bdc9a79da3358404d227
Will Studabaker <will.studabaker@gmail.com>
2020-11-10 15:02:34 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -4,7 +4,7 @@ require 'pry'
 class Scraper
 
   def self.scrape_index_page(index_url)
-    html = open("https://learn-co-curriculum.github.io/student-scraper-test-page/index.html")
+    html = open(index_url)
     doc = Nokogiri::HTML(html)
   end
 
