8a7c119df8865a0937fee4d7113bf399c781d1ad
Will Studabaker <will.studabaker@gmail.com>
2020-11-11 13:53:04 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -9,7 +9,7 @@ class Student
   end
 
   def self.create_from_collection(students_array)
-    students_array.each{|s_a| Student.new(s_a)}
+    students_array.each{|s| Student.new(s)}
   end
 
   def add_student_attributes(attributes_hash)
